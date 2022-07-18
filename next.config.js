5e752568822742d1fe70276b52443669c62e223d
yanyu <yuyan802@outlook.com>
2022-07-19 02:20:22 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   swcMinify: true,
   images: {
     domains: ["i.ytimg.com", "s2.loli.net", "lh3.googleusercontent.com"],
